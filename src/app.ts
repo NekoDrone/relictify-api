c73ce8a599983458ad48f8a6ae122e4703f1ea6f
Sylfr <sylfr.serenity.tan@gmail.com>
2023-11-14 21:11:37 +0800

    More logging for debugging.

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,23 @@ import {routes} from "./routes"
 import {Authorizer, IAuthorizer} from "./middleware/auth";
 import {ILogger, Logger} from "./middleware/logger";
 import {PROD_ENV} from "./exports/consts";
+import * as bodyParser from "body-parser"
 
 const app = express();
 const port = process.env.PORT ?? 8080;
+
 const logger: ILogger = new Logger();
 const auth: IAuthorizer = new Authorizer();
 console.log(`Production environment is ${PROD_ENV}`);
 
 app.listen(port,  () => {
     console.log("Relictify API serving requests.");
+});
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    console.log(`NOT MIDDLEWARE Request body is: ${req.body ?? 'undefined'}`);
+    console.log(`NOT MIDDLEWARE App Auth Key is ${req.body.authKey ?? 'undefined'}`);
 })
-app.use(express.json())
 app.use(logger.logRequest);
 app.use(auth.authHandler);
 
