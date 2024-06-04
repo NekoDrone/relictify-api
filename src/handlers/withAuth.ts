11b68d01147dad327b9aa3bd32f8dc98cd88f813
Sylfr Serenity Tan <98162476+NekoDrone@users.noreply.github.com>
2024-06-04 18:20:09 +0800

    refactor: auth middleware

--- src/handlers/withAuth.ts
+++ src/handlers/withAuth.ts
@@ -1,15 +1,16 @@
-import { error, IRequest } from "itty-router";
+import { error, IRequest, IRequestStrict } from "itty-router";
 
 const appAuthKey = process.env.APP_AUTH_KEY ?? "undefined";
 const encodedAuth = Buffer.from(appAuthKey).toString('base64')
 
-const withAuth = (request: IRequest) => {
-    const reqToken = request.headers.get('Authorization')
-    console.log(`Request token is ${reqToken}`)
-    
-    if (reqToken != encodedAuth) return error(401, 'Incoming request not authorised. Access denied.')
-    
-    console.log("Incoming request authorised.")
+type AuthBody = {
+    relictifyAuth: string;
+}
+
+const withAuth = async ( request: IRequest ) => {
+    const reqToken = request.content as AuthBody;
+    console.log(JSON.stringify(reqToken))
+    if (reqToken.relictifyAuth != encodedAuth) return error(401, 'Incoming request not authorised. Please provide the correct "relictifyAuth" token in the request body')
 }
 
 export default withAuth
