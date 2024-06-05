25becbf9e49f1dba66285ac04ad55549f11eb82e
Sylfr Serenity Tan <98162476+NekoDrone@users.noreply.github.com>
2024-06-05 18:39:07 +0800

    fix: authorization header

--- src/handlers/withAuth.ts
+++ src/handlers/withAuth.ts
@@ -1,16 +1,11 @@
-import { error, IRequest, IRequestStrict } from "itty-router";
+import { error, IRequest } from "itty-router";
 
 const appAuthKey = process.env.APP_AUTH_KEY ?? "undefined";
 const encodedAuth = Buffer.from(appAuthKey).toString('base64')
-
-type AuthBody = {
-    relictifyAuth: string;
-}
-
 const withAuth = async ( request: IRequest ) => {
-    const reqToken = request.headers.get('Authorization - Relictify')
-    if (!reqToken) return error(400, '\'Authorization - Relictify\' header not found. Please set it in the request.')
-    if (reqToken != encodedAuth) return error(401, 'Incoming request not authorised. Please ensure the correct \'Authorization - Relictify\' header is set properly.')
+    const reqToken = request.headers.get('Authorization')
+    if (!reqToken) return error(400, '\'Authorization\' header not found. Please set it in the request.')
+    if (reqToken != encodedAuth) return error(401, 'Incoming request not authorised. Please ensure the correct \'Authorization\' header is set properly.')
 }
 
 export default withAuth
