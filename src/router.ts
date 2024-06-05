4f9a29f6ee52b5cd2c8012f47d827772fca27b78
Sylfr Serenity Tan <98162476+NekoDrone@users.noreply.github.com>
2024-06-05 15:19:25 +0800

    refactor: remove withContent router since no post method

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,11 @@
-import { AutoRouter, withContent } from "itty-router";
+import { AutoRouter } from "itty-router";
 import withAuth from "./handlers/withAuth";
 import { allChars, charById } from "@/routes/characters";
 
 const router = AutoRouter();
 
 router
-    .all('*', withContent, withAuth)
+    .all('*', withAuth)
     .get('/characters', allChars)
     .get('/characters/:id', ({ id }) => charById(id))
     .get('/relics', () => ({"foo": "bar"}))
