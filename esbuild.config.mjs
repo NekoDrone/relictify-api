f38c5549e11232870c97d97e917de8293f355a68
Sylfr Serenity Tan <98162476+NekoDrone@users.noreply.github.com>
2024-06-05 18:33:31 +0800

    fix, ci: esbuild entry

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -2,7 +2,7 @@ import * as esbuild from 'esbuild'
 import path from "path";
 
 await esbuild.build({
-    entryPoints: [path.resolve('src/app.ts')],
+    entryPoints: [path.resolve('src/index.ts')],
     bundle: true,
     platform: 'node',
     target: ['node20'],
