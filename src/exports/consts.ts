c8b7a8d717eccecca23655d779bf2495adb780c1
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2024-07-20 20:54:29 +0800

    refactor: restart project and init supabase

--- src/exports/consts.ts
+++
@@ -1,5 +0,0 @@
-export const PROD_ENV = process.env.PROD_ENV ?? throwNewError("PROD_ENV not set");
-
-function throwNewError(msg: string): never{
-    throw new Error(msg);
-}
--- src/routes/characters.ts
+++
@@ -1,9 +0,0 @@
-import { getAllCharacters, getCharacterById } from "@/handlers/queryCharacters";
-
-export async function allChars() {
-    return getAllCharacters()
-}
-
-export async function charById(id: string) {
-    return getCharacterById(parseInt(id))
-}
